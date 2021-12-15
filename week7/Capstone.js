abcce094a2dd1b8f3d453ed5a97d3ed49b5cac00
bunicholas <nicholascarroll123@gmail.com>
2021-12-14 23:21:30 -0500

    M

---
+++ week7/Capstone.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import '';
+import 
+function get(){
+    axios.get()
+    .then()
+}
+axios.get()
+axios.put()
+axios.post()
+axios.delete()
